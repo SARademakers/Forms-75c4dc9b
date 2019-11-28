31b88213e128409475a2aaa8bceb031d9b2f0eb9
SARademakers <2049137@talnet.nl>
2019-11-28 15:09:15 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,13 @@
+<html>
+    <body> 
+        <form action="form.php" method="get" class="form">
+            <div class="form">
+                <label for="email">Email: </label>
+                <input type="email" name="email" id="email" required>
+            </div>
+                <div class="form">
+                <input type="submit" value="Verzenden">
+            </div>
+        </form>
+    </body>
+</html>
